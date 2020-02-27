791aa2a4276e96bf3dffd35dce3b42618b15fd88
Sandra <a0960529965@gmail.com>
2020-02-27 18:05:21 +0800

    map 定位

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,153 @@
 <template>
-  <div id="app">
-    <div class="row no-gutters">
-      <div class="col-sm-3">
-        <selectIfo />
-      </div>
-      <div class="col-sm-9">
-        <VueLeaflet id="map"></VueLeaflet>
-        <infoPopup></infoPopup>
-      </div>
+    <div id="app">
+        <div class="row no-gutters">
+            <div class="col-sm-3">
+                <div class="select-ifo">
+                    <div class="toolbox" v-if="cityName.length">
+                        <div class="sticky-top bg-white shadow-sm p-2">
+                            <div class="form-group d-flex">
+                                <label for="cityName" class="mr-2 col-form-label text-right">縣市</label>
+                                <div class="flex-fill">
+                                    <select id="cityName" class="form-control" v-model="select.city" @change='select.area=""'>
+                                        <option value="">-- Select One --</option>
+                                        <option :value="c.CityName" v-for="(c, index) in cityName" :key="index">{{ c.CityName }}</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-group d-flex">
+                                <label for="area" class="mr-2 col-form-label text-right">地區</label>
+                                <div class="flex-fill">
+                                    <select id="area" class="form-control">
+                                        <option value="">-- Select One --</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <p class="mb-0 small text-muted text-right ">
+                                請先選擇區域查看（綠色表示還有口罩）
+                            </p>
+                        </div>
+                        <ul class="list-group">
+                            <template>
+                                <a class="list-group-item text-left ">
+                                    <h3>藥局名稱</h3>
+                                    <p class="mb-0">
+                                        成人口罩：1 | 兒童口罩：2
+                                    </p>
+                                    <p class="mb-0">
+                                        地址：<a href="https://www.google.com.tw/maps/place/..." target="_blank" title="Google Map">
+                                            地址</a>
+                                    </p>
+                                </a>
+                            </template>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-9">
+                <div id="map">
+                    <l-map class="map" :zoom="zoom" :center="center" :attribution="attribution" @updateMap='updateMap'>
+                        <l-tile-layer :url="url"></l-tile-layer>
+                        <l-marker :icon="icon">
+                            <l-popup>Hello!</l-popup>
+                        </l-marker>
+                    </l-map>
+                    <v-marker-cluster></v-marker-cluster>
+                </div>
+                <!-- <infoPopup></infoPopup> -->
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import VueLeaflet from "./components/VueLeaflet.vue";
-import infoPopup from "./components/infoPopup.vue";
-import selectIfo from "./components/selectIfo.vue";
+    // import VueLeaflet from "./components/VueLeaflet.vue";
+    // import infoPopup from "./components/infoPopup.vue";
+    // import selectIfo from "./components/selectIfo.vue";
+    //引入
+    import cityName from "./assets/CityCountyData.json";
+    // 引入地圖
+    import L from "leaflet";
+    console.log(L);
+    //使用變數
+    let osmMap = {};
+    export default {
+        name: "app",
+        // components: {
+        //     VueLeaflet,
+        //     selectIfo,
+        //     infoPopup
+        // },
+        data() {
+            return {
+                isPopupShow: false,
+                data: [],
+                cityName,
+                select: {
+                    city: '臺北市',
+                    area: '大安區',
+                },
+            };
+        },
+        methods: {
+            updateMap() {
+                //1--取遠端程式碼,將台北市跑出來,使用filter將資料列出
+                const pharmacies = this.data.filter(
+                    (pharmacy) => (pharmacy.properties.county === this.select.city)
+                );
+                // console.log(pharmacies);
+                //2-- 確保台北市圖資加到畫面上
+                pharmacies.forEach((pharmacy) => {
+                    //解構寫法
+                    const { properties, geometry } = pharmacy;
+                    //marker座標位置[0][1]放入
+                    L.marker([geometry.coordinates[1], geometry.coordinates[0]]).addTo(osmMap);
+                    console.log(properties);
+                });
+            }
+        },
+        mounted() {
+            const url =
+                "https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json";
+            this.$http.get(url)
+                .then(response => {
+                    this.data = response.data.features;
+                });
+            osmMap = L.map('map', {
+                center: [25.03, 121.55],// 台北市區的經緯度（地圖中心）
+                zoom: 15,// 地圖預設尺度
+            });
+            // 新增圖資圖層（OSM 圖資）
+            const osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+            L.tileLayer(osmUrl, {                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+            }).addTo(osmMap);
 
-export default {
-  name: "app",
-  components: {
-    VueLeaflet,
-    selectIfo,
-    infoPopup
-  },
-  data() {
-    return {
-      isPopupShow: false,
-      data: []
+            // L.marker([25.03, 121.55]).addTo(osmMap);
+        },
     };
-  },
-  mounted() {
-    const url =
-      "https://raw.githubusercontent.com/kiang/pharmacies/master/json/points.json";
-    this.$http.get(url).then(response => {
-      this.data = response.data.features;
-    });
-  }
-};
 </script>
 
 <style lang="scss">
-@import "./style/reset";
-@import "bootstrap/scss/bootstrap";
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-  #map {
-    height: 100vh;
-  }
-}
+    @import "./style/reset";
+    @import "bootstrap/scss/bootstrap";
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        color: #2c3e50;
+        #map {
+            height: 100vh;
+        }
+        .select-ifo {
+            .highlight {
+                background: #e9ffe3;
+            }
+            .toolbox {
+                height: 100vh;
+                overflow-y: auto;
+                a {
+                    cursor: pointer;
+                }
+            }
+        }
+    }
 </style>
--- src/components/VueLeaflet.vue
+++ src/components/VueLeaflet.vue
@@ -1,55 +1,79 @@
 <template>
-  <div class="vue-leaflet">
-    <l-map class="map" :zoom="zoom" :center="center" :attribution="attribution">
-      <l-tile-layer :url="url"></l-tile-layer>
-      <l-marker :lat-lng="markerLatLng">
-        <l-popup>Hello!</l-popup>
-      </l-marker>
-    </l-map>
-    <v-marker-cluster></v-marker-cluster>
-  </div>
+    <div class="vue-leaflet">
+        <l-map class="map" :zoom="zoom" :center="center" :attribution="attribution">
+            <l-tile-layer :url="url"></l-tile-layer>
+            <l-marker :icon="icon">
+                <l-popup>Hello!</l-popup>
+            </l-marker>
+        </l-map>
+        <v-marker-cluster></v-marker-cluster>
+    </div>
 </template>
 
 <script>
-// 引入地圖
-// import L from "leaflet";
-//引入地圖套件相關
-import { LMap, LTileLayer, LMarker, LPopup } from "vue2-leaflet";
 
-import Vue2LeafletMarkerCluster from "vue2-leaflet-markercluster";
+    //引入地圖套件相關
+    // import { LMap, LTileLayer, LMarker, LPopup } from "vue2-leaflet";
+    // import Vue2LeafletMarkerCluster from "vue2-leaflet-markercluster";
+    // 引入地圖
+    import L from "leaflet";
+    console.log(L);
+    export default {
+        name: "vueLeaflet",
+        components: {
 
-export default {
-  name: "vueLeaflet",
-  components: {
-    // L,
-    LMap,
-    LTileLayer,
-    LMarker,
-    LPopup,
-    "v-marker-cluster": Vue2LeafletMarkerCluster
-  },
-  data() {
-    return {
-      url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-      zoom: 16,
-      center: [24.9836633, 121.5346939],
-      markerLatLng: [24.9836633, 121.5346939],
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            LMap,
+            LTileLayer,
+            LMarker,
+            LPopup,
+            "v-marker-cluster": Vue2LeafletMarkerCluster
+        },
+        data() {
+            return {
+                zoom: 17,
+                url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                attribution:
+                    '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+                // 上面一樣 下面開始替換 Marker Icon
+                icon_hasAllMask: L.icon({
+                    iconUrl: require("@/assets/images/Icon_location_green.svg"),
+                    iconSize: [70, 100],
+                    iconAnchor: [22, 94]
+                }),
+                icon_onlyOneMask: L.icon({
+                    iconUrl: require("@/assets/images/Icon_location_orange.svg"),
+                    iconSize: [70, 100],
+                    iconAnchor: [22, 94]
+                }),
+                icon_noMask: L.icon({
+                    iconUrl: require("@/assets/images/Icon_location_red.svg"),
+                    iconSize: [70, 100],
+                    iconAnchor: [22, 94]
+                }),
+            }
+        },
+        mounted() {
+            // osmMap = L.map('map', {
+            //     center: [25.03, 121.55],// 台北市區的經緯度（地圖中心）
+            //     zoom: 15,// 地圖預設尺度
+            // });
+            // // 新增圖資圖層（OSM 圖資）
+            // const osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+            // L.tileLayer(osmUrl, {                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+            // }).addTo(osmMap);
+        }
     };
-  }
-};
 </script>
 
 <style lang="scss">
-// @import "~leaflet.markercluster/dist/MarkerCluster.css";
-// @import "~leaflet.markercluster/dist/MarkerCluster.Default.css";
-.vue-leaflet {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  .map {
-    height: 100%;
-  }
-}
+    // @import "~leaflet.markercluster/dist/MarkerCluster.css";
+    // @import "~leaflet.markercluster/dist/MarkerCluster.Default.css";
+    .vue-leaflet {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        .map {
+            height: 100%;
+        }
+    }
 </style>
